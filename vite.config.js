20185e58dceaeb7e25b6e560306a70ce8addd978
angela300 <ligoangela.22@gmail.com>
2026-02-13 22:56:55 +0300

    Fix vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+const repoName = 'angelaPortf'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`, 
   plugins: [react(), tailwindcss()],
+
 })
